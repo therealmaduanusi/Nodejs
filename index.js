392c3ee39441067230f07c1d6ea270e89dc89843
Victor Maduanusi <maduanusivictor2020@gmail.com>
2024-05-18 21:08:20 +0100

    new file:   .gitignore 	new file:   README.MD 	modified:   index.js 	deleted:    script.js 	modified:   views/index.ejs

--- index.js
+++ index.js
@@ -5,7 +5,24 @@ const app = express();
 const port = 3000;
 
 // Sample data array for posts
-const posts = [];
+const posts = [
+  {
+    id: 1,
+    title: "The Rise of Decentralized Finance",
+    content:
+      "Decentralized Finance (DeFi) is an emerging and rapidly evolving field in the blockchain industry. It refers to the shift from traditional, centralized financial systems to peer-to-peer finance enabled by decentralized technologies built on Ethereum and other blockchains. With the promise of reduced dependency on the traditional banking sector, DeFi platforms offer a wide range of services, from lending and borrowing to insurance and trading.",
+    author: "Alex Thompson",
+    date: "Sat May 16 2024 20:52:54 GMT+0100 (West Africa Standard Time)"
+  },
+  {
+    id: 2,
+    title: "The Impact of Artificial Intelligence on Modern Businesses",
+    content:
+      "Artificial Intelligence (AI) is no longer a concept of the future. It's very much a part of our present, reshaping industries and enhancing the capabilities of existing systems. From automating routine tasks to offering intelligent insights, AI is proving to be a boon for businesses. With advancements in machine learning and deep learning, businesses can now address previously insurmountable problems and tap into new opportunities.",
+    author: "Mia Williams",
+    date: "Sat May 18 2024 9:52:54 GMT+0100 (West Africa Standard Time)"
+  }
+];
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended:true}));
@@ -13,23 +30,25 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.get('/', (req, res) => {
-    res.render('index.ejs', { posts });
-    // res.send('hello')
+  res.render('index.ejs', { posts: posts });
+  // res.send('hello')
 })
+
 app.post('/submit', (req, res) => {
-    const infoHead = req.body.header;
-    const infoContent = req.body['content'];
-    // Add the new post to the posts array
-    posts.push({ title: infoHead, content: infoContent });
+  const infoHead = req.body.header;
+  const infoContent = req.body['content'];
+  let getDate = new Date();
+  // Add the new post to the posts array
+  posts.push({ title: infoHead, content: infoContent, date: getDate });
 
-    // res.render('index.ejs', { posts });
-    res.redirect('/')
-    // res.send('hello')
-    // console.log(req.body);
-    // console.log(req.body['content']);
+  res.render('index.ejs', { posts: posts });
+  // res.redirect('/')
+  // res.send('hello')
+  // console.log(req.body);
+  // console.log(req.body['content']);
 
-    // console.log(posts); // Log the updated posts array
+  // console.log(posts); // Log the updated posts array
 })
 app.listen(port, (req, res) => {
-    console.log(`Listening at port ${port}`);
+  console.log(`Listening at port ${port}`);
 })
